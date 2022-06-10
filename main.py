aa844f99138dcb4094fce4d52d0c764fa9c9f8f3
Paddington1 <>
2022-06-10 03:40:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+# Setborderline and Title of form
+print("[=======================]")
+print("[ Welcome to the CarMax ]")
+print("[  Customization form!  ]")
+print("[=======================]")
+print()
+print()
+# Instructions
+print("[For multiple choice questions you may enter in 'a' - 'e' *OR* write out your answer] - THANKS!")
+print()
+# Section - A - Heading
+print("~~  " "MAKE & MODEL" " ~~ ")
+print()
+# Question 1 Title & list of options
+print("1.) Type of car:")
+print("   A.) SUV")
+print("   B.) Sedan")
+print("   C.) Compact")
+print("   D.) Convertable")
+print("   E.) Truck")
+print()
+# Prompt user to enter their choice
+CarType = input("    * Please enter or write your choice here: ")
+print()
+# Question 2 Title & list of options
+print("2.) Make of car: ")
+print("   A. Ford")
+print("   B. Chevrolet")
+print("   C. Toyota")
+print("   D. Lincoln")
+print("   E. Mercedes-Benz")
+print()
+#Prompt user to enter their choice
+CarMake = input("    * Please enter or write your choice here: ")
+print()
+print()
+print()
+# Section - B - Heading
+print("~~  ""CUSTOMIZING THE EXTERIOR""  ~~ ")
+print()
+# Question 3 Title & User Prompt
+print("3.) Would you like your car to have a sunroof?")
+print()
+SunRoof = input("    * Please type 'Yes' or 'No': ")
+print()
+# Qestion 4 Title & User Prompt
+print("4.) Choice of exterior color paint: ")
+print()
+Color = input("    * Please write your prefered color: ")
+print()
+print()
+print()
+# Section - C - Heading
+print("~~  ""SUBSCRIPTIONS""  ~~")
+print()
+# Question 5 Title & User Prompt
+print("5.) Would you like to add a three year warranty?")
+Warranty = input("Please enter 'Yes' or 'No': ")
+print()
+print("6.) Would you like to add one year of SiriusXM") 
+Radio = input("    * Please enter 'Yes' or 'No': ")
+print()
+print()
+print()
+print()
+# Summary of Answers 
+print("---- ""CAR ORDER SUMMARY"" ----")
+print()
+print(f"Car Type  : {CarType}")
+print(f"Car Make  : {CarMake}")
+print(f"Sunroof   : {SunRoof}")
+print(f"Ext paint : {Color}")
+print(f"Warranty  : {Warranty}")
+print(f"SiriusXM  : {Radio}")
+
+
+
+
+
+
+
+
+
+# Print options to Question 1
+# Prompt user to enter their choice
+
+
